7e759caf54da80a1413aeb63bc83576b428f5e57
Tachapol Chaimongkolsup <tachapol.false@gmail.com>
2025-10-29 14:47:52 +0700

    fix: wrap SafeScreen in AuthProvider for improved context management

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,14 +1,16 @@
 import SafeScreen from "@/components/SafeScreen";
 import { Slot } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-
+import { AuthProvider } from "../src/contexts/AuthContext";
 
 export default function RootLayout() {
   return (
-    <SafeScreen>
-      <Slot /> 
-      <StatusBar style="dark" />
-    </SafeScreen>
+    <AuthProvider>
+      <SafeScreen>
+        <Slot />
+        <StatusBar style="dark" />
+      </SafeScreen>
+    </AuthProvider>
   );
 }
 
